ceffec84f0a213d690b8e9345eb9af169c7d8ab2
Richard Garside <729381+NogginBox@users.noreply.github.com>
2013-08-19 10:15:49 +0100

    Original tagged module

---
+++ CurrentContentItemHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Orchard;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Handlers;
+
+namespace NogginBox.Tagged
+{
+	public class CurrentContentItemHandler : ContentHandler
+	{
+		private readonly IWorkContextAccessor _workContextAccessor;
+
+		public CurrentContentItemHandler(IWorkContextAccessor workContextAccessor)
+		{
+			_workContextAccessor = workContextAccessor;
+		}
+
+		protected override void BuildDisplayShape(BuildDisplayContext context)
+		{
+			if (context.DisplayType == "Detail")
+			{
+				var workContext = _workContextAccessor.GetContext();
+				var contentItems = workContext.GetState<List<IContent>>("ContentItems");
+				if (contentItems == null)
+				{
+					workContext.SetState("ContentItems", contentItems = new List<IContent>());
+				}
+
+				contentItems.Add(context.ContentItem);
+			}
+		}
+	}
+}
---
+++ WithTagsRuleProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Orchard;
+using Orchard.ContentManagement;
+using Orchard.Mvc;
+using Orchard.Tags.Models;
+using Orchard.Widgets.Services;
+
+namespace NogginBox.Tagged
+{
+	public class WithTagsRuleProvider : IRuleProvider
+	{
+		private readonly IHttpContextAccessor _httpContextAccessor;
+		private readonly IWorkContextAccessor _workContextAccessor;
+
+		public WithTagsRuleProvider(IHttpContextAccessor httpContextAccessor, IWorkContextAccessor workContextAccessor)
+		{
+			_httpContextAccessor = httpContextAccessor;
+			_workContextAccessor = workContextAccessor;
+		}
+
+		public void Process(RuleContext ruleContext)
+		{
+			if (!String.Equals(ruleContext.FunctionName, "tagged", StringComparison.OrdinalIgnoreCase))
+				return;
+
+			var tag = Convert.ToString(ruleContext.Arguments[0]);
+			var workContext = _workContextAccessor.GetContext();
+			var contentItems = workContext.GetState<List<IContent>>("ContentItems");
+
+			if (contentItems != null)
+			{
+				var taggedContent = contentItems.Where(c => c.As<TagsPart>() != null);
+
+				if (taggedContent.Any(c => c.As<TagsPart>().CurrentTags.Any(t => t.TagName == tag)))
+				{
+					ruleContext.Result = true;
+					return;
+				}
+			}
+
+			ruleContext.Result = false;
+		}
+	}
+}
