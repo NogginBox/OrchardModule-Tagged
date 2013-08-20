98db7a63bf3152112af57046b77e24747aeb1492
Richard Garside <729381+NogginBox@users.noreply.github.com>
2013-08-20 15:40:55 +0100

    Tags no longer need to be case sensative.

--- WithTagsRuleProvider.cs
+++ WithTagsRuleProvider.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Orchard;
 using Orchard.ContentManagement;
-using Orchard.Mvc;
 using Orchard.Tags.Models;
 using Orchard.Widgets.Services;
 
@@ -11,18 +10,17 @@ namespace NogginBox.Tagged
 {
 	public class WithTagsRuleProvider : IRuleProvider
 	{
-		private readonly IHttpContextAccessor _httpContextAccessor;
 		private readonly IWorkContextAccessor _workContextAccessor;
 
-		public WithTagsRuleProvider(IHttpContextAccessor httpContextAccessor, IWorkContextAccessor workContextAccessor)
+		public WithTagsRuleProvider(IWorkContextAccessor workContextAccessor)
 		{
-			_httpContextAccessor = httpContextAccessor;
 			_workContextAccessor = workContextAccessor;
 		}
 
 		public void Process(RuleContext ruleContext)
 		{
-			if (!String.Equals(ruleContext.FunctionName, "tagged", StringComparison.OrdinalIgnoreCase))
+			if (! (String.Equals(ruleContext.FunctionName, "tagged", StringComparison.OrdinalIgnoreCase)
+				|| String.Equals(ruleContext.FunctionName, "tag", StringComparison.OrdinalIgnoreCase)) )
 				return;
 
 			var tag = Convert.ToString(ruleContext.Arguments[0]);
@@ -33,7 +31,7 @@ namespace NogginBox.Tagged
 			{
 				var taggedContent = contentItems.Where(c => c.As<TagsPart>() != null);
 
-				if (taggedContent.Any(c => c.As<TagsPart>().CurrentTags.Any(t => t.TagName == tag)))
+				if (taggedContent.Any(c => c.As<TagsPart>().CurrentTags.Any(t => String.Equals(t.TagName, tag, StringComparison.OrdinalIgnoreCase))))
 				{
 					ruleContext.Result = true;
 					return;
